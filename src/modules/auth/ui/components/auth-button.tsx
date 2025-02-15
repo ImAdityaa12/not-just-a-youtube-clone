1d9f623967e5f9dbefb2e14a01c946f0ae78781f
ImAdityaa12 <adityagupta1291@gmail.com>
2025-02-16 02:44:09 +0530

    Implement authentication button with Clerk integration for sign-in and user display

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,16 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react";
 import React from "react";
 
 const AuthButton = () => {
   return (
-    <Button
-      variant={"outline"}
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500/20 rounded-full shadow-none transition-all"
-    >
-      <UserCircleIcon />
-      Sign In
-    </Button>
+    <>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant={"outline"}
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500/20 rounded-full shadow-none transition-all"
+          >
+            <UserCircleIcon />
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </>
   );
 };
 
