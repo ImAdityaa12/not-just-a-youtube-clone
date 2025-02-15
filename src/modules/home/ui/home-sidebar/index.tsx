38156dfc620f7cf6910897ccc408aa0d4d74540e
ImAdityaa12 <adityagupta1291@gmail.com>
2025-02-15 19:17:55 +0530

    Add SidebarMainSection component to home sidebar and update imports

--- src/modules/home/ui/home-sidebar/index.tsx
+++ src/modules/home/ui/home-sidebar/index.tsx
@@ -1,9 +1,9 @@
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import SidebarMainSection from "./main-section";
+import SidebarMainSection from "./sidebar-main-section";
 export const HomeSidebar = () => {
   return (
     <Sidebar className="top-16 z-40 border-none">
-      <SidebarContent className="bg-red-500">
+      <SidebarContent className="">
         <SidebarMainSection />
       </SidebarContent>
     </Sidebar>
