e62113f2d28299029a3aaf4dc635b085dc55545f
ImAdityaa12 <adityagupta1291@gmail.com>
2025-02-17 22:49:48 +0530

    Implement tRPC integration with query handling and update layout for TRPCProvider

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { trpc } from "@/trpc/client";
 import React from "react";
 
 const page = () => {
-  return <div>I will show some videos here!</div>;
+  const greeting = trpc.hello.useQuery({
+    text: "world",
+  });
+  if (!greeting.data) return <div>Loading...</div>;
+  return <div>I will show some videos here! {greeting.data?.greeting}</div>;
 };
 
 export default page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TRPCProvider } from "@/trpc/client";
+
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +20,9 @@ export default function RootLayout({
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          <TRPCProvider> {children}</TRPCProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
