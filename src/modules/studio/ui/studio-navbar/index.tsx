8eb80df5d5bb0dab05f4089b7fc65817c3fac457
ImAdityaa12 <adityagupta1291@gmail.com>
2025-04-10 13:59:27 +0530

    refactor(studio-navbar): update logo link and text for clarity

--- src/modules/studio/ui/studio-navbar/index.tsx
+++ src/modules/studio/ui/studio-navbar/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Link from "next/link";
 import AuthButton from "@/modules/auth/ui/components/auth-button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import StudioNavbarSearch from "./studio-navbar-search";
 
 const StudioNavbar = () => {
   return (
@@ -11,19 +10,15 @@ const StudioNavbar = () => {
       <div className="flex items-center gap-4 w-full">
         <div className="flex items-center flex-shrink-0 gap-4">
           <SidebarTrigger />
-          <Link href={"/"}>
+          <Link href={"/studio"}>
             <div className="flex items-center gap-1">
               <Image src="/logo.svg" alt="Logo" width={32} height={32} />
               <p className="font-semibold text-xl tracking-tighter">
-                NextTube.ai
+                Studio
               </p>
             </div>
           </Link>
         </div>
-        <div className="flex-1 flex items-center justify-center max-w-[720px] mx-auto">
-          <StudioNavbarSearch />
-        </div>
-
         <div className="flex-shrink-0 items-center flex gap-4">
           <AuthButton />
         </div>
