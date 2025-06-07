6e32302f95d64963b6c19881d63554d5f062bd5f
ImAdityaa12 <adityagupta1291@gmail.com>
2025-06-07 22:20:39 +0530

    feat(video-owner): enhance video owner component with user info and subscription
    - Add UserInfo component to display user details
    - Implement SubscriptionButton for non-owner users
    - Add edit button for video owners
    - Improve layout and styling of the component

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import { VideoGetOneOutput } from '../../types';
+import Link from 'next/link';
+import UserAvatar from '@/components/user-avatar';
+import { useAuth } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+import SubscriptionButton from '../subscriptions/ui/components/subscription-button';
+import UserInfo from '../users/ui/components/user-info';
 
 interface VideoOwnerProps {
     user: VideoGetOneOutput['user'];
+    videoId: string;
 }
 
-const VideoOwner = ({ user }: VideoOwnerProps) => {
-    return <div>{user.name}</div>;
+const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
+    const { userId: clerkUserId } = useAuth();
+    return (
+        <div className="flex items-center  justify-between sm:justify-start gap-3 min-w-0">
+            <Link href={`/users/${user.id}`}>
+                <div className="flex items-center gap-3 min-w-0">
+                    <UserAvatar
+                        size={'lg'}
+                        imageUrl={user.imageUrl}
+                        name={user.name}
+                    />
+                    <UserInfo />
+                    <span className="text-sm text-muted-foreground line-clamp-1">
+                        {/* TODO: Add Subscribers Count Properly*/}
+                        {0} Subscribers
+                    </span>
+                </div>
+            </Link>
+            {user.clerkId === clerkUserId ? (
+                <Button variant={'secondary'}>
+                    <Link href={`/studio/videos/${videoId}`}>Edit Video</Link>
+                </Button>
+            ) : (
+                <SubscriptionButton
+                    disabled={false}
+                    isSubscribed={false}
+                    size={'sm'}
+                    onClick={() => {}}
+                />
+            )}
+        </div>
+    );
 };
 
 export default VideoOwner;
---
+++ src/modules/videos/ui/subscriptions/ui/components/subscription-button.tsx
@@ -0,0 +1,33 @@
+import { Button, ButtonProps } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+interface SubscriptionButtonProps {
+    onClick: ButtonProps['onClick'];
+    disabled: boolean;
+    isSubscribed: boolean;
+    className?: string;
+    size?: ButtonProps['size'];
+}
+
+const SubscriptionButton = ({
+    onClick,
+    disabled,
+    isSubscribed,
+    className,
+    size,
+}: SubscriptionButtonProps) => {
+    return (
+        <Button
+            variant={isSubscribed ? 'secondary' : 'default'}
+            size={size}
+            onClick={onClick}
+            disabled={disabled}
+            className={cn('rounded-full', className)}
+        >
+            {isSubscribed ? 'Subscribed' : 'Subscribe'}
+        </Button>
+    );
+};
+
+export default SubscriptionButton;
