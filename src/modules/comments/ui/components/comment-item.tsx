2ed22b695e718808de20daec0c4bc16fb1c9c07b
ImAdityaa12 <adityagupta1291@gmail.com>
2025-07-13 11:53:14 +0530

    feat(comments): improve comment item layout and add timestamp
    Add timestamp display using date-fns and restructure the comment item layout to include user name and comment value in a more organized way. Also fix the user profile link path.

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { CommentsGetManyOutput } from '../../types';
 import UserAvatar from '@/components/user-avatar';
+import { formatDistanceToNow } from 'date-fns';
 
 interface CommentItemProps {
     comment: CommentsGetManyOutput[number];
@@ -10,12 +11,27 @@ export const CommmentItem = ({ comment }: CommentItemProps) => {
     return (
         <div>
             <div className="flex gap-4">
-                <Link href={`users/${comment.userId}`} />
+                <Link href={`/users/${comment.userId}`} />
                 <UserAvatar
                     size={'lg'}
                     imageUrl={comment.user.imageUrl}
                     name={comment.user.name}
                 />
+                <div className="flex flex-1 min-w-[0] flex-col">
+                    <Link href={`/users/${comment.userId}`}>
+                        <div className="flex items-center gap-2 mb-0.5">
+                            <span className="font-medium text-sm pb-0.5">
+                                {comment.user.name}
+                            </span>
+                            <span className="text-xs text-muted-foreground">
+                                {formatDistanceToNow(comment.createdAt, {
+                                    addSuffix: true,
+                                })}
+                            </span>
+                        </div>
+                    </Link>
+                    <p className="text-sm">{comment.value}</p>
+                </div>
             </div>
         </div>
     );
